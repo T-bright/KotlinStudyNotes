30b359d0474a3b7c23a00d2e31d1a5ea6d823ecf
T-bright <t_bright@163.com>
2019-05-06 10:19:16 +0800

    添加泛型和反射

---
+++ app/src/main/java/tsw/kotlin/newcharacteristics/a_kotlingrammar/f_genericparadigm/GenericParadigm.kt
@@ -0,0 +1,96 @@
+package tsw.kotlin.newcharacteristics.a_kotlingrammar.f_genericparadigm
+
+import android.util.Log
+
+open class GenericParadigm {
+    open var value = "Generic_Paradigm"
+
+    class TestOneClass<T>(t: T) {
+        var value = t
+    }
+
+    class TestTwoClass<T : GenericParadigm>(t: T) {
+        var value = t
+
+    }
+
+    open class TestThreeClass : GenericParadigm() {
+        override var value = "TestThreeClass"
+    }
+
+    class TestFourClass : TestThreeClass() {
+        override var value = "TestFourClass"
+    }
+
+    //逆变,其或其父类
+    class TestFiveClass<in T>(t: T) {
+        var value = "TestFiveClass"
+        fun getType(t: T) {//逆变可以做入参，不能做返回值
+
+        }
+    }
+
+    //协变,其或其子类
+    class TestSixClass<out T>(val t: T) {
+        var value = "TestSixClass"
+        fun getType(): T {//协变不可以做入参，可以用来做返回值
+            return t
+        }
+    }
+
+
+    fun oneTest() {
+        var testOne = TestOneClass<String>("aaaa")
+        log(testOne.value)
+        testOne = TestOneClass("bbbbb")
+        log(testOne.value)
+
+        var ss = TestOneClass<String?>(null)//
+        log(ss.value.toString())
+    }
+
+
+    //泛型约束
+    fun twoTest() {
+        //此时接收null的就会报错，编译无法通过
+//        TestTwoClass(null)
+
+        //此时TestTwoClass只能接收GenericParadigm类型和GenericParadigm子类的类型
+        var testTwoClass = TestTwoClass<TestThreeClass>(TestThreeClass())
+        var testThreeClass = testTwoClass.value
+        log(testThreeClass.value)
+
+        copy(listOf<String>("1", "5", "6"), "4")
+
+    }
+
+    //对于多个约束上界，可以用where表示。下面的T的上界是CharSequence和Comparable<T>，而String的上界（父类）是CharSequence和Comparable<String>，所以可以穿String类型的
+    fun <T> copy(list: List<T>, thresold: T): List<String>
+            where  T : CharSequence, T : Comparable<T> {
+        return list.filter { it > thresold }.map { it.toString() }
+    }
+
+    //型变
+    fun threeTest() {
+        //逆变类型限定其或其父类
+        var genericParadigmNB = TestFiveClass<GenericParadigm>(GenericParadigm())
+        var testThreeClassNB = TestFiveClass<TestThreeClass>(TestThreeClass())
+        var testFourClassNB = TestFiveClass<TestFourClass>(TestFourClass())
+        testThreeClassNB = genericParadigmNB
+        //下面这行代码编译不通过，因为逆变类型限定其或其父类，而TestFourClass是TestThreeClass的子类，所以编译不通过
+//        testThreeClassNB = testFourClassNB
+
+
+        //协变类型限定其或其子类
+        var genericParadigmXB = TestSixClass<GenericParadigm>(GenericParadigm())
+        var testThreeClassXB = TestSixClass<TestThreeClass>(TestThreeClass())
+        var testFourClassXB = TestSixClass<TestFourClass>(TestFourClass())
+        testThreeClassXB = testFourClassXB
+        //下面这行代码编译不通过，因为协变类型限定其或其子类，而GenericParadigm是TestThreeClass的父类，所以编译不通过
+//        testThreeClassXB = genericParadigmXB
+    }
+
+    fun log(message: String) {
+        Log.e("GenericParadigm", message)
+    }
+}
---
+++ app/src/main/java/tsw/kotlin/newcharacteristics/a_kotlingrammar/f_genericparadigm/GenericParadigmActivity.kt
@@ -0,0 +1,13 @@
+package tsw.kotlin.newcharacteristics.a_kotlingrammar.f_genericparadigm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import tsw.kotlin.newcharacteristics.R
+
+class GenericParadigmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generic_paradigm)
+    }
+}
